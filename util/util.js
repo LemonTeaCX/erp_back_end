2f752c2b5c53efd7666a83fdfcc2e070916812e4
lemon <1406428663@qq.com>
2019-03-10 23:48:08 +0800

    Update util.js

--- util/util.js
+++ util/util.js
@@ -5,9 +5,9 @@ class Util {
 	}
 	treeMenu(arr = []) {
     if (!arr.length || arr.length === 0) return;
-    // 删除 所有 subMenu,以防止多次调用
+    // 删除 所有 nodes,以防止多次调用
     arr.forEach(function (item) {
-      delete item.subMenu;
+      delete item.nodes;
     });
 
     // 将数据存储为 以 id 为 KEY 的 map 索引数据列
@@ -19,10 +19,10 @@ class Util {
     let val = [];
     arr.forEach(function (item) {
       // 以当前遍历项，的pid,去map对象中找到索引的id
-      let parent = map[item.parentId];
+      let parent = map[item.parent_id];
       // 好绕啊，如果找到索引，那么说明此项不在顶级当中,那么需要把此项添加到，他对应的父级中
       if (parent) {
-        (parent.subMenu || ( parent.subMenu = [] )).push(item);
+        (parent.nodes || ( parent.nodes = [] )).push(item);
       } else {
         //如果没有在map中找到对应的索引ID,那么直接把 当前的item添加到 val结果集中，作为顶级
         val.push(item);
